4de4ad03f410bce2ba378ab81a057fd4ded0a440
whart78 <williamhart77@icloud.com>
2025-03-17 18:34:09 -0500

    scheduling algorithms

--- Project1/src/project1/SJF.java
+++ Project1/src/project1/SJF.java
@@ -1,12 +1,21 @@
 package project1;
+import java.util.*;
+
+// Shortest Job First (SJF) Scheduling Algorithm
 public class SJF extends SchedulingAlgorithm {
-      public SJF (List<PCB> queue) {
-		super("SJF", queue);
-	}
+    public SJF(List<PCB> queue) {
+        super("SJF", queue);
+    }
 
-
-      public PCB pickNextProcess() {
-		// TODO Auto-generated method stub
-    	  return null;
-      }
+    @Override
+    public PCB pickNextProcess() {
+        if (readyQueue.isEmpty()) return null;
+        PCB shortestJob = readyQueue.get(0);
+        for (PCB proc : readyQueue) {
+            if (proc.getCpuBursts().get(0) < shortestJob.getCpuBursts().get(0)) {
+                shortestJob = proc;
+            }
+        }
+        return shortestJob;
+    }
 }
